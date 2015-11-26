4c91f40a3654555331aca9df93018edfb2def19d
ChrisCWerner <ccwerner96@gmail.com>
2015-11-26 12:11:37 -0200

    Adicionadas assertivas que garantem a direção do fluxo.

--- src/funcoes.c
+++ src/funcoes.c
@@ -5,11 +5,12 @@
 #include <curses.h>
 
 /************************************************************************************************************
-* Nome da função: Inicializa_Record;
-* Descrição: Inicializa a estrutura que irá armazenar os dados dos relatórios.
-* Assertivas de Entrada: nenhuma
-* Assertivas de Saída: 
-*			Record *Registro != NULL (registro alocado na memória)
+* Nome da funcao: Inicializa_Record;
+* Descricao: Inicializa a estrutura que ira armazenar os dados dos relatorios.
+* Assertivas de Entrada:
+*			nenhum
+* Assertivas de Saida: 
+*			Record *Registro != NULL (registro alocado na memoria)
 *************************************************************************************************************/
 Record *Inicializa_Record(void){
 	Record *rec = (Record *) malloc(sizeof(Record));
@@ -31,16 +32,16 @@ Record *Inicializa_Record(void){
 }
 
 /**********************************************************************************************************
-* Nome da função: Localiza_Paths;
-* Descrição: Faz as ligações entre os Geradores e adapatadores e entre adapatadores e cidades,
-*	através das interconexões.
+* Nome da funcao: Localiza_Paths;
+* Descricao: Faz as ligacoes entre os Geradores e adapatadores e entre adapatadores e cidades,
+*	através das interconexoes.
 * Assertivas de entrada: 
 *			Listas criadas:
 *			Listas->p_cidade != NULL;
 *			Listas->p_gerador != NULL;
 *			Listas->p_interc != NULL;
 *			Listas->p_adapter != NULL;
-* Assertivas de saída:
+* Assertivas de saida:
 *			nenhum
 ************************************************************************************************************/
 void Localiza_Paths(Listas *inicio){
@@ -116,18 +117,25 @@ void Localiza_Paths(Listas *inicio){
 				aux3->vaic = 'C';
 			}
 			
+			//Condicoes de fluxo de recursos:
+			if(aux3->vemc == 'G')
+				assert(aux3->vaic == 'A');	//Garante que toda interc. que sai de um gerador chegue em um adaptador.
+			if(aux3->vemc == 'A')
+				assert(aux3->vaic == 'C');	//Garante que toda interc. que sai de um adaptador chegue em uma cidade.
+			//Caso fique preso em uma dessas assertivas, revise o arquivo de entrada.
+			
 			aux1 = aux1->prox;
 		}
 		aux3 = aux3->prox;
 	}
 }
 /*******************************************************************************************************************
-* Nome da função: Insere_Irmão;
-* Descrição: Insere na lista de interconexão as outras interconexões que saem do mesmo adaptador.
+* Nome da funcao: Insere_Irmao;
+* Descricao: Insere na lista de interconexao as outras interconexoes que saem do mesmo adaptador.
 * Assertivas de entrada:
 *			Interc *novo_elem != NULL
 *			Inetrc *top != NULL
-* Assertivas de saída:
+* Assertivas de saida:
 *			Interc *saida != NULL && Interc *saida = top;
 ********************************************************************************************************************/
 Interc *Insere_Irmao(Interc *novo_elem, Interc *top){
@@ -139,15 +147,15 @@ Interc *Insere_Irmao(Interc *novo_elem, Interc *top){
 	return top;
 }
 /************************************************************************************************************************
-* Nome da função: Calcula_Cap_Total;
-* Descrição: Calcula a capacidade total que todas as interconexões podem transportar;
+* Nome da funcao: Calcula_Cap_Total;
+* Descricao: Calcula a capacidade total que todas as interconexoes podem transportar;
 * Assertivas de Entrada:
 *			Listas criadas:
 *			Listas->p_cidade != NULL;
 *			Listas->p_gerador != NULL;
 *			Listas->p_interc != NULL;
 *			Listas->p_adapter != NULL;
-* Assertivas de Saída:
+* Assertivas de Saida:
 *			int capacidade_total != 0;
 *************************************************************************************************************************/
 int Calcula_Cap_Total(Interc *inicio){
@@ -166,8 +174,8 @@ int Calcula_Cap_Total(Interc *inicio){
 }
 
 /************************************************************************************************************************
-* Nome da função: Calcula_Cap_Total;
-* Descrição: Calcula a capacidade relativa que cada interconexão podem transportar;
+* Nome da funcao: Calcula_Cap_Total;
+* Descricao: Calcula a capacidade relativa que cada interconexao podem transportar;
 * Assertivas de Entrada:
 *			Listas criadas:
 *			Listas->p_cidade != NULL;
@@ -175,7 +183,7 @@ int Calcula_Cap_Total(Interc *inicio){
 *			Listas->p_interc != NULL;
 *			Listas->p_adapter != NULL;
 *			int total = Capacidade_total;
-* Assertivas de Saída:
+* Assertivas de Saida:
 *			int capacidade_total != 0;
 *************************************************************************************************************************/
 int Calcula_Rel_Flow(Interc *inicio, int total){
@@ -195,8 +203,8 @@ int Calcula_Rel_Flow(Interc *inicio, int total){
 }
 
 /**************************************************************************************************************
-* Nome da função: Calcula_Fluxo;
-* Descrição: Calcula a sobre de recurso nos adapatadores;
+* Nome da funcao: Calcula_Fluxo;
+* Descricao: Calcula a sobre de recurso nos adapatadores;
 * Assertivas de entrada: 
 *			void *inicio != NULL;
 *			caracter identificador;
@@ -259,8 +267,8 @@ int Calcula_Fluxo(void *inicio, char id){
 }
 
 /**************************************************************************************************************
-* Nome da função: Gerencia_Sobra;
-* Descrição: Faz um gerenciamento da sobre de recursos tanto de adpatadores e geradores, para as interconexões
+* Nome da funcao: Gerencia_Sobra;
+* Descricao: Faz um gerenciamento da sobre de recursos tanto de adpatadores e geradores, para as interconexoes
 *		que saem dele;
 * Assertivas de entrada: 
 *			void *inicio != NULL;
@@ -350,8 +358,8 @@ void Gerencia_Sobra(void *inicio, char id){
 	}
 }
 /**************************************************************************************************************
-* Nome da função:Fluxo_Adpat;
-* Descrição: calcula o fluxo de energia que chega ao adpatador;
+* Nome da funcao:Fluxo_Adpat;
+* Descricao: calcula o fluxo de energia que chega ao adpatador;
 * Assertivas de entrada: 
 *			void *inicio != NULL;
 * Assertivas de saida:
@@ -377,8 +385,8 @@ void Fluxo_Adapt(Interc *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Fluxo_City;
-* Descrição: Calcula o fluxo de recurso que chega nas cidades;
+* Nome da funcao: Fluxo_City;
+* Descricao: Calcula o fluxo de recurso que chega nas cidades;
 * Assertivas de entrada: 
 *			Interc *inicio != NULL;
 *			Record *Registro != NULL (Registro alocado)
@@ -407,8 +415,8 @@ void Fluxo_City(Interc *inicio, Record *rec){
 }
 
 /**************************************************************************************************************
-* Nome da função: Verifica_Falhas
-* Descrição: Verifica se houve falhas nas interconexões
+* Nome da funcao: Verifica_Falhas
+* Descricao: Verifica se houve falhas nas interconexoes
 * Assertivas de entrada: 
 *			Interc *inicio != NULL;
 *			Record *Registro != NULL;
@@ -448,8 +456,8 @@ void Verifica_Falhas(Interc *inicio, Record *rec){
 }
 
 /**************************************************************************************************************
-* Nome da função: Maneja_Falhas
-* Descrição: faz o gerenciamento das falhas nos geradores e adpatadores, modificando a variável booleana,
+* Nome da funcao: Maneja_Falhas
+* Descricao: faz o gerenciamento das falhas nos geradores e adpatadores, modificando a variavel booleana,
 *		para indicar falha;
 * Assertivas de entrada: 
 *			Interc *inicio != NULL;
@@ -485,8 +493,8 @@ void Maneja_Falhas(Interc *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Zera_Fluxo
-* Descrição: Zera o fluxo das interconexões
+* Nome da funcao: Zera_Fluxo
+* Descricao: Zera o fluxo das interconexoes
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -523,8 +531,8 @@ void Zera_Fluxo(Listas *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Distribui_Recursos;
-* Descrição: Faz a distribuição dos recursos dos geradores para os adaptadores, cidades através das interconexões.
+* Nome da funcao: Distribui_Recursos;
+* Descricao: Faz a distribuicao dos recursos dos geradores para os adaptadores, cidades através das interconexoes.
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -586,8 +594,8 @@ void Distribui_Recursos(Listas *inicio){
 	rec->tempo_total++;
 }
 /**************************************************************************************************************
-* Nome da função: Examina_Cidades
-* Descrição: Calcula o tempo em que a cidade está sem recurso;
+* Nome da funcao: Examina_Cidades
+* Descricao: Calcula o tempo em que a cidade esta sem recurso;
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -598,25 +606,17 @@ void Examina_Cidades(Listas *inicio){
 	Record *rec = inicio->p_record;
 	Cidade *city = inicio->p_cidade;
 	
-//	int segundocontado = 0, segundocontado30 = 0;
-
 	while(city != NULL){
 		
 		if(city->fluxo < city->recurso_necessario){
-//			if(segundocontado == 0){
 				rec->tempo_cidades_sem_recurso++;
-//				segundocontado = 1;
-//			}
 			if(city->sem_rec == 0){
 				city->sem_rec = 1;
 				rec->cidades_sem_recurso++;
 			}
 		}
 		if(city->fluxo < 0.3*city->recurso_necessario){
-//			if(segundocontado30 == 0){
 				rec->tempo_sem_30porcento++;
-//				segundocontado30 = 1;
-//			}
 	
 			if(city->sem_rec < 2){
 				city->sem_rec = 2;
@@ -628,8 +628,8 @@ void Examina_Cidades(Listas *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Total_Elementos
-* Descrição: Calcula o total de cidades e geradores
+* Nome da funcao: Total_Elementos
+* Descricao: Calcula o total de cidades e geradores
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -653,8 +653,8 @@ void Total_Elementos(Listas *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Tamanho_Interc
-* Descrição: Calcula o tamanho de todas as interconexões juntas
+* Nome da funcao: Tamanho_Interc
+* Descricao: Calcula o tamanho de todas as interconexoes juntas
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -681,8 +681,8 @@ float Tamanho_Interc(Interc *inicio){
 }
 
 /**************************************************************************************************************
-* Nome da função: Relatorio
-* Descrição: Grava no arquivo .txt o relatório final com todos so dados solicitados pelo podf do trabalho
+* Nome da funcao: Relatorio
+* Descricao: Grava no arquivo .txt o relatorio final com todos so dados solicitados pelo pdf do trabalho
 * Assertivas de entrada: 
 *			Listas *inicio != NULL;
 * Assertivas de saida:
@@ -773,4 +773,3 @@ void Relatorio(Listas *inicio){
 	getch();
 	endwin();	
 }*/
-
